47e03ef34d2ebe77fa9bbbd96aca7198bdc96ad0
tauhid <tauhid.islam4@gmail.com>
2024-02-06 18:34:36 -0500

    ArrayList updated

--- src/org/dc/java/arrays/Main.java
+++ src/org/dc/java/arrays/Main.java
@@ -1,14 +1,43 @@
 package org.dc.java.arrays;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws IOException {
-        int[] numbers = new int[5];
+        /*int[] numbers = new int[5];
         for (int i=0; i<5; i++) {
             System.out.println(numbers[i]);
-        }
+        }*/
+        
+        String[] countries = new String[3];
+        countries[0] = "England";
+        countries[1] = "Canada";
+        countries[2] = "Mexico";
+        
+        System.out.println(countries[countries.length-1]);
+
+        List<String> countriesList = new ArrayList<>();
+        countriesList.add("Canada");
+        countriesList.add("England");
+        countriesList.add("Mexico");
+
+        System.out.println(countriesList);
+
+        countriesList.add("Egypt");
+        System.out.println(countriesList);
+        System.out.println(countriesList.getLast());
+
+        List<String> newCountriesList = Arrays.asList(countries);
+        System.out.println(newCountriesList);
+        Arrays.sort(countries);
+        List<String> newCountriesList2 = Arrays.asList(countries);
+        System.out.println(newCountriesList2);
+
+
     }
 }
